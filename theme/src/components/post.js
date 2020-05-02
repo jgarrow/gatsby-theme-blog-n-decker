6c60292424d9333e741c3bb1b249eb3035f75962
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-01 22:59:47 -0600

    Create comment form

--- theme/src/components/post.js
+++ theme/src/components/post.js
@@ -1,16 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import {
   Styled,
   css,
   ThemeProvider,
   ColorMode,
   InitializeColorMode,
-  Label,
-  Input,
-  Textarea,
-  Button,
-  Box,
 } from "theme-ui"
+
+import { Label, Input, Textarea, Button, Box } from "@theme-ui/components"
 
 import PostFooter from "gatsby-theme-blog/src/components/post-footer"
 import Layout from "gatsby-theme-blog/src/components/layout"
@@ -18,8 +15,8 @@ import SEO from "gatsby-theme-blog/src/components/seo"
 // import { FacebookProvider, Comments } from "react-facebook"
 
 import blogTheme from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/index"
-import waves from "./waves"
-import Body from "./body"
+import waves from "gatsby-theme-deck-n-blog/src/components/waves"
+import Body from "gatsby-theme-deck-n-blog/src/components/body"
 import deepmerge from "deepmerge"
 
 const Post = ({
@@ -33,6 +30,11 @@ const Post = ({
   previous,
   next,
 }) => {
+  const [comment, setComment] = useState({
+    name: "",
+    comment: "",
+  })
+
   if (previous) {
     previous.slug = `${previous.slug}`
   }
@@ -41,43 +43,81 @@ const Post = ({
     next.slug = `${next.slug}`
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const handleChange = (e) => {
+    setComment({
+      ...comment,
+      [e.target.name]: e.target.value,
+    })
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formdata = new FormData()
+    formdata.set("fields[name]", comment.name)
+    formdata.set("fields[message]", comment.essage)
+    const json = {}
+    formdata.forEach((value, prop) => (json[prop] = value))
+    const formBody = Object.keys(json)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(json[key])
+      )
+      .join("&")
+
+    // in the repo, create a folder named 'comments'
+    const response = await fetch(
+      "https://deck-n-blog-comments.herokuapp.com/v2/entry/jgarrow/gatsby-theme-deck-n-blog/demo/master/comments/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: formBody,
+      }
+    )
+    console.log(response)
+  }
+
+  console.log(
+    "deepmerge(blogTheme, { styles: { waves } }): ",
+    deepmerge(blogTheme, { styles: { waves } })
+  )
+
   return (
-    <ThemeProvider theme={deepmerge(blogTheme, { styles: { waves } })}>
-      {/* <ColorMode /> */}
-      <InitializeColorMode />
-      <Layout location={location} title={title}>
-        <SEO title={post.title} description={post.excerpt} />
-        <main>
-          <Styled.h1>{post.title}</Styled.h1>
-          <Styled.p
-            css={css({
-              fontSize: 1,
-              mt: -3,
-              mb: 3,
-            })}
-          >
-            {post.date}
-          </Styled.p>
-          <Body body={post.body} /> hello123
-          {/* <FacebookProvider appId="123456789">
+    // <ThemeProvider theme={deepmerge(blogTheme, { styles: { waves } })}>
+    // {/* <ThemeProvider theme={blogTheme}> */}
+    // {/* <ColorMode /> */}
+    // <InitializeColorMode />
+    <Layout location={location} title={title}>
+      <SEO title={post.title} description={post.excerpt} />
+      <main>
+        <Styled.h1>{post.title}</Styled.h1>
+        <Styled.p
+          css={css({
+            fontSize: 1,
+            mt: -3,
+            mb: 3,
+          })}
+        >
+          {post.date} hello
+        </Styled.p>
+        <Body body={post.body} />
+        {/* <FacebookProvider appId="123456789">
             <Comments href="http://www.facebook.com" />
           </FacebookProvider> */}
-          <Box as="form" onSubmit={handleSubmit}>
-            <Label htmlFor="name">Name</Label>
-            <Input name="name" mb={3} />
-            <Label htmlFor="comment">Comment</Label>
-            <Textarea name="comment" rows="6" mb={3} />
-            <Button>Submit</Button>
-          </Box>
-        </main>
-
-        <PostFooter {...{ previous, next }} />
-      </Layout>
-    </ThemeProvider>
+        <Box as="form" onSubmit={handleSubmit}>
+          <Label htmlFor="name">Name</Label>
+          <Input name="name" mb={3} onChange={handleChange} />
+          <Label htmlFor="comment">Comment</Label>
+          <Textarea name="comment" rows="6" mb={3} onChange={handleChange} />
+          <div
+            className="g-recaptcha"
+            data-sitekey="6Ld_PfEUAAAAAJQX_kD8eYwSI53qJPcFfE9M4Vpv"
+          ></div>
+          <Button>Submit</Button>
+        </Box>
+      </main>
+      <PostFooter {...{ previous, next }} />
+    </Layout>
+    // </ThemeProvider>
   )
 }
 
