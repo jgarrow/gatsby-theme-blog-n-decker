e4ccdd28f940e754cafc2d1bc1659a7d7a7ba3dc
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-03 21:19:34 -0600

    More cleanup. Partial fix for deck fonts in posts. Still not what it's supposed to be, but at least different than the post body font

--- theme/src/gatsby-plugin-theme-ui/index.js
+++ theme/src/gatsby-plugin-theme-ui/index.js
@@ -1,13 +1,10 @@
 // import wavesTheme from "gatsby-theme-waves/src/gatsby-plugin-theme-ui/index"
 import blogTheme from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/index"
-  import decksTheme from "gatsby-theme-mdx-deck/src/gatsby-plugin-theme-ui/index"
+// import decksTheme from "gatsby-theme-mdx-deck/src/gatsby-plugin-theme-ui/index"
 // import merge from "deepmerge"
 
 // export default merge.all([decksTheme, blogTheme, wavesTheme])
 
-console.log(decksTheme)
-console.log(blogTheme)
-
 // export default { ...decksTheme }
-export default { ...blogTheme}
+export default { ...blogTheme }
 // export default merge(blogTheme, wavesTheme)
