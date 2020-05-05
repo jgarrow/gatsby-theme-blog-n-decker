a3fdc6c4f5e1140661fb9369fb3b5ca3b20d2903
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-05 12:27:23 -0600

    Merge themes to fix issues with slides

--- src/gatsby-theme-mdx-deck/components/deck.js
+++ src/gatsby-theme-mdx-deck/components/deck.js
@@ -1,10 +1,13 @@
 import React from "react"
-import { ThemeProvider } from "theme-ui"
+import { useThemeUI } from "theme-ui"
 import mdxDeckTheme from "gatsby-theme-mdx-deck/src/gatsby-plugin-theme-ui/index"
 import Deck from "gatsby-theme-mdx-deck/src/components/deck"
+import merge from "deepmerge"
 
-export default props => (
-  <ThemeProvider theme={mdxDeckTheme}>
-    <Deck {...props} />
-  </ThemeProvider>
-)
+export default ({ theme, ...props }) => {
+  const { theme: baseTheme } = useThemeUI()
+
+  const mergedTheme = merge.all([baseTheme, mdxDeckTheme, theme])
+
+  return <Deck theme={mergedTheme} {...props} />
+}
